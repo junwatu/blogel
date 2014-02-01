dd9475ac4ca2cb5bfc07788bdd5fec505fe790d9
Equan Pr <equan.p@gmail.com>
2014-02-01 16:40:52 +0700

    medium editor options.

--- public/js/pagedekui.js
+++ public/js/pagedekui.js
@@ -1,5 +1,12 @@
  /**
  *	Main Application JS
  */
-
- var editor = new MediumEditor('.editable');
+var elements = document.querySelectorAll('.editable'),
+    editor = new MediumEditor(elements, {
+    anchorInputPlaceholder: 'Type a link',
+    buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote','pre'],
+    firstHeader: 'h1',
+    secondHeader: 'h2',
+    delay: 0,
+    targetBlank: true
+});
