11be15687fc7efb4f299e333a8d5a8fbc326c34e
Equan Pr <equan.p@gmail.com>
2014-02-02 07:09:38 +0700

    prettify

--- public/js/pagedekui.js
+++ public/js/pagedekui.js
@@ -1,12 +1,12 @@
  /**
- *	Main Application JS
- */
-var elements = document.querySelectorAll('.editable'),
-    editor = new MediumEditor(elements, {
-    anchorInputPlaceholder: 'Type a link',
-    buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote','pre'],
-    firstHeader: 'h1',
-    secondHeader: 'h2',
-    delay: 0,
-    targetBlank: true
-});
+  *	Main Application JS
+  */
+ var elements = document.querySelectorAll('.editable'),
+     editor = new MediumEditor(elements, {
+         anchorInputPlaceholder: 'Type a link',
+         buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote', 'pre', 'image'],
+         firstHeader: 'h1',
+         secondHeader: 'h2',
+         delay: 0,
+         targetBlank: true
+     });
