69f04e3e4e94c1a8e7ce2e5d43e81ca0fb32cafe
Equan Pr <eq@junwatu.com>
2014-02-03 10:41:16 +0700

    button event listener.

--- public/js/pagedek.js
+++ public/js/pagedek.js
@@ -1,17 +1,45 @@
  /**
-  *	Main Application JS
+  * Main Application JS
   */
  var elements = document.querySelectorAll('.editable'),
      editor = new MediumEditor(elements, {
          anchorInputPlaceholder: 'Type a link',
-         buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote', 'pre', 'image'],
+         buttons: ['bold', 'italic', 'underline', 'anchor', 'header1', 'header2', 'quote', 'pre'],
          firstHeader: 'h1',
          secondHeader: 'h2',
          delay: 0,
          targetBlank: true
      });
 
-$('#save-btn').on('click', function(){
-    var content = editor.serialize();
-    console.log(content);
-});
+ /**
+  * Using JQuery
+  */
+
+ $('#new-btn').on('click', function(){
+    console.log('New Document clicked!');
+ });
+
+ $('#publish-btn').on('click', function() {
+     console.log('publish-btn clicked!');
+ });
+
+ $('#save-btn').on('click', function() {
+     var content = editor.serialize(),
+         json_content = {
+             "title": content['element-0'],
+             "content": content['element-1'],
+             "date-create": Date.now(),
+             "last-update": Date.now(),
+             "status": "draft"
+         }
+
+     console.log(json_content);
+ });
+
+ $('#setting-btn').on('click', function() {
+     console.log('setting-btn clicked!');
+ });
+
+ $('#trash-btn').on('click', function() {
+     console.log('trash-btn clicked!');
+ });
