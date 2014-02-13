7590f3756662fe28db7cd7ae1a5dc8bf469c583c
Equan Pr <equan.p@gmail.com>
2014-02-13 07:18:51 +0700

    running app and open it on chrome browser.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,39 @@
 /**
-*  Gulp build
-*/
-var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	sass = require('gulp-sass'),
-	paths = {
-		scss: 'scss/**/*.scss',
-		scssdest: 'public/css'
-	};
+ *  Gulp build
+ */
+ 
+"use strict";
 
-gulp.task('scss', function(){
-	gulp.src(paths.scss)
-		.pipe(sass())
-		.pipe(gulp.dest(paths.scssdest));
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    paths = {
+        scss: 'scss/**/*.scss',
+        scssdest: 'public/css'
+    },
+    exec = require('child_process').exec;
+
+gulp.task('scss', function() {
+    gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(paths.scssdest));
 });
 
-gulp.task('watch', function(){
-	gulp.watch(paths.scss, ['scss'])
-})
 
-gulp.task('default',['scss', 'watch']);
+gulp.task('server', function() {
+    exec('supervisor pagedek.js', function(err, stdout, stderr) {
+        if (err != null) console.log(stderr);
+        console.log(stdout);
+    });
+
+    exec('google-chrome http://localhost:5555', function(err, stdout, stderr) {
+    	if(err != null) console.log(stderr);
+    	console.log(stdout);
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.scss, ['scss'])
+});
+
+gulp.task('default', ['scss', 'server', 'watch']);
