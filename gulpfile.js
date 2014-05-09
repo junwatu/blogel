ab39aa02f939b5f5d4a8442a57bcbb3fb712dd95
Equan Pr <equan.p@gmail.com>
2014-05-09 16:07:31 +0700

    watch and uglify main javascript file too.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,22 @@
  
 "use strict";
 
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    sass = require('gulp-sass'),
-    paths = {
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var exec = require('child_process').exec;
+var uglify = require('gulp-uglify');
+
+var paths = {
         scss: 'scss/**/*.scss',
-        scssdest: 'public/css'
-    },
-    exec = require('child_process').exec;
+        scssdest: 'public/css',
+        js: 'public/js/pagedekui.js',
+        jsdest: 'public/js/dist' 
+    };
+
+/**
+ * Tasks
+ */
 
 gulp.task('scss', function() {
     gulp.src(paths.scss)
@@ -19,6 +27,11 @@ gulp.task('scss', function() {
         .pipe(gulp.dest(paths.scssdest));
 });
 
+gulp.task('uglify', function(){
+    gulp.src(paths.js)
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.jsdest));
+});
 
 gulp.task('server', function() {
     exec('supervisor pagedek.js', function(err, stdout, stderr) {
@@ -33,7 +46,8 @@ gulp.task('server', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch(paths.scss, ['scss'])
+    gulp.watch(paths.scss, ['scss']);
+    gulp.watch(paths.js, ['uglify']);
 });
 
-gulp.task('default', ['scss', 'watch']);
+gulp.task('default', ['watch']);
