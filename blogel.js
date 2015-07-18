b0f2f28fb12f0830bf9ccd13a349220c6000c77f
Equan Pr <equan.p@gmail.com>
2015-07-18 09:38:54 +0700

    cleanup license description

--- blogel.js
+++ blogel.js
@@ -1,11 +1,11 @@
 /**++++++++++++++
-*   Pagedek
+*  BLOGEL
 *++++++++++++++++
 *
 *
 The MIT License (MIT)
 
-Copyright (c) 2014 Equan Pr.
+Copyright (c) 2015 Equan Pr.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
