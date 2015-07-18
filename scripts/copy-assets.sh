af385d3bac073ab23577e46784b2d9e119686526
Equan Pr <byte@equan.me>
2015-07-19 02:44:47 +0700

    cleanup

--- scripts/copy-assets.sh
+++ scripts/copy-assets.sh
@@ -4,4 +4,4 @@ if [ ! -d 'public/css/themes' ] ; then
     mkdir public/css/themes
 fi
 cp -v ./node_modules/medium-editor/dist/css/medium-editor.css public/css
-cp -rvf ./node_modules/medium-editor/dist/css/themes/default*.css public/css/themes
+cp -rvf ./node_modules/medium-editor/dist/css/themes/default.css public/css/themes
