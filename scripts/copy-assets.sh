064012e1f9e2566b5d5cb64ae600ccab7b603cb9
Equan Pr <byte@equan.me>
2015-07-18 16:09:14 +0700

    copy client js and css files

---
+++ scripts/copy-assets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -v ./node_modules/medium-editor/dist/js/medium-editor.min.js public/js 
+mkdir public/css/themes
+cp -v ./node_modules/medium-editor/dist/css/medium-editor.css public/css
+cp -rvf ./node_modules/medium-editor/dist/css/themes/default*.css public/css/themes
