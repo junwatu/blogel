6f3dd9a60b181fe08970c5141a6ac615b26b46aa
Equan Pr <equan.p@gmail.com>
2015-07-18 18:14:14 +0700

    check if themes directory is exist

--- scripts/copy-assets.sh
+++ scripts/copy-assets.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp -v ./node_modules/medium-editor/dist/js/medium-editor.min.js public/js 
-mkdir public/css/themes
+if [ ! -d 'public/css/themes' ] ; then
+    mkdir public/css/themes
+fi
 cp -v ./node_modules/medium-editor/dist/css/medium-editor.css public/css
 cp -rvf ./node_modules/medium-editor/dist/css/themes/default*.css public/css/themes
