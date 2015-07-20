b75d1433d15452236c100325e09133c79dd72678
Equan Pr <equan.p@gmail.com>
2015-07-21 02:03:54 +0700

    always use strict mode

--- server/logger/index.js
+++ server/logger/index.js
@@ -4,17 +4,18 @@
 * The MIT License (MIT)
 * Copyright (c) 2014 Equan Pr.
 */
+'use strict';
 var winston = require('winston');
 var config = require('../config');
-var root = require('../util');
+var root = require('../util').path();
 
 exports.Logger = new (winston.Logger)({
     transports: [
         new (winston.transports.File)( {
-        filename: root.path()+config.get('logger:filename'),
-        maxsize: 1048576,
-        maxFiles: 5,
-        handleExceptions: true
+            filename: root+config.get('logger:filename'),
+            maxsize: 1048576,
+            maxFiles: 5,
+            handleExceptions: true
         }),
         new (winston.transports.Console)({level: 'error'})
     ]
