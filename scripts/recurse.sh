de5379f44190f466eb3c7c18aa77a275442479e0
Equan Pr <equan.p@gmail.com>
2015-07-20 15:01:15 +0700

    bash script to transpile recursively

---
+++ scripts/recurse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Create temporary directory dan transpile es6 js files.
+# 
+# MIT (c) 2015  Equan Pr.
+
+set RECURSE_DIR=server
+set TEMP_DIR=.tmp
+set NODE_TRANSPILER=scripts/tools/node-transpiler
+
+transpile_recurse() {
+ for i in "$1"/*;do
+    if [ -d "$i" ];then
+        if [ ! -d "$TEMP/$i" ] ; then
+           y=$i
+           mkdir -p "$TEMP/${y#*/}"
+        fi
+        recurse "$i"
+    elif [ -f "$i" ]; then
+        x=$i
+        node "$NODE_TRANSPILER"/bin/transpile "$i" > "$TEMP/${x#*/}"
+    fi
+ done
+}
+
+transpile_recurse server
