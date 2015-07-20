c3aa17f615c66af6b6bb6996bb484aca7784cffc
Equan Pr <equan.p@gmail.com>
2015-07-20 21:18:12 +0700

    load app from tmp dir

--- blogel.js
+++ blogel.js
@@ -25,5 +25,6 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *
 */
-
-module.exports = require('./lib/pring');
+'use strict';
+const TEMP_DIR = './tmp';
+module.exports = require(`${TEMP_DIR}/pring`);
