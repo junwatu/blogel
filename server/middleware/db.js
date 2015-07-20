753e0213bb8070ed11dd2483bbeced077064d662
Equan Pr <equan.p@gmail.com>
2015-07-21 02:02:38 +0700

    string literal

--- server/middleware/db.js
+++ server/middleware/db.js
@@ -1,14 +1,16 @@
+'use babel';
 /**
 * NeDB Database
 *  
 * The MIT License (MIT)
-* Copyright (c) 2014 Equan Pr.
+* Copyright (c) 2015 Equan Pr.
 */
 'use strict';
 var Datastore = require('nedb');
-var root = require('../util');
+var root = require('../util').path();
+
 var db = new Datastore({
-    filename: root.path()+'/db/blogel.db',
+    filename: `${root}/db/blogel.db`,
     autoload: true
 });
 
