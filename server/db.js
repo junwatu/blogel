d6fabe0542d5c56ab5ef71c43857ed49ad56ea3b
Equan Pr <equan.p@gmail.com>
2015-07-25 14:08:40 +0700

    Add database and tables exist checking functions
    Signed-off-by: Equan Pr <byte@equan.me>

--- server/db.js
+++ server/db.js
@@ -17,7 +17,7 @@ var r = require('rethinkdb');
 var root = require('./util').path();
 var options = { host: 'localhost', port: 28015, authkey:'', db:DB_NAME}; 
 
-import type { Post } from './core/types.js';
+import type { Post, Author } from './core/types.js';
 
 var samplePost:Post = {
     title: 'Post',
@@ -30,28 +30,101 @@ var samplePost:Post = {
     tags: ['hello', 'dummy']
 }
 
-// initialize database
 asyncInitDb();
 
 function asyncInitDb(log:boolean = true) {
     getDbConnection()
     .then((conn) => {
-        return createDatabase(conn);    
+        dbIsExist(conn).then((status) => {
+            if(!status.exist) {
+                return createDatabase(conn);
+            }
+            return p(status.conn);
+        })
+        .then( (response) => {
+            postTableIsExist(response.conn).then((status) => {
+                if(!status.exist) {
+                    return createPostTable(status.conn);        
+                }
+                return p(status.conn)
+            })
+            .then( (response) => {
+                authorsTableIsExist(response.conn).then((status) => {
+                    if(!status.exist) {
+                        return createAuthorsTable(response.conn);        
+                    }
+                    return p(status.conn);
+                })
+                .then( (res) => {
+                    console.log('Database Ok');
+                })
+                .catch( (err) => {
+                    console.log(err.message);
+                    process.exit(1);
+                });
+            })
+        })
     })
-    .then( (response) => {
-        return createPostTable(response.conn);
+}
+
+function p(conn: any): Promise {
+    return new Promise((resolve, reject) => {
+        resolve({conn:conn});
     })
-    .then( (response) => {
-        return createAuthorsTable(response.conn);
-    })
-    .then( (res) => {
-        console.log(res);
-    })
-    .catch( (err) => {
-        console.log('Unable to connect to rethinkdb');
-        console.log(err.message);
-        process.exit(1);
+}
+
+function dbIsExist(conn: any): Promise {
+    return new Promise((resolve, reject) => {
+        r.dbList().run(conn, (err,list) => {
+            if(err) {
+                reject(err);
+            } else {
+                var blogel: boolean = false;
+                list.forEach((v,i,a) => {
+                    if(v === DB_NAME) {
+                        blogel = true;
+                    }
+                })
+                blogel ? resolve({ exist:true, conn:conn}):resolve({ exist:false, conn:conn});
+            }
+        });
     });
+}
+
+function postTableIsExist(conn: any): Promise {
+    return new Promise( (resolve, reject) => {
+        r.db(DB_NAME).tableList().run(conn, (err, table) => {
+            if(err) {
+                reject(err);
+            } else { 
+                var post: boolean = false;
+                table.forEach( (v,i,a) => {
+                    if(v === POST_TABLE_NAME) {
+                        post = true;
+                    }
+                })
+                post ? resolve({ exist:true, conn:conn}): resolve({ exist:false, conn:conn});
+            }
+        });
+    });   
+}
+
+function authorsTableIsExist(conn: any): Promise {
+    return new Promise((resolve, reject) => {
+        r.db(DB_NAME).tableList().run(conn, (err,table) => {
+            if(err) {
+                reject(err);
+            } else { 
+                var author: boolean = false;
+                table.forEach((v,i,a) => {
+                    if(v === AUTHORS_TABLE_NAME) {
+                        author = true;
+                    }
+                })
+                author ? resolve({ exist:true, conn:conn}) : resolve({ exist:false, conn:conn});
+            }
+        });
+    });   
 }
 
 function createDatabase(conn: any): Promise {
@@ -98,6 +171,7 @@ function getPostByAuthor(authorId: string): Array<Post> {
 
 function savePost(post:Post): boolean {
     var savePostStatus: boolean = true;
+    r.db(DB_NAME).table(POST_TABLE_NAME).run();                                                                                                                                                             
     return savePostStatus;
 }
 
