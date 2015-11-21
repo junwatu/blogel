ad32cc4a8f690986ccfb8b587856ed370a162be0
Equan Pr <equan.p@gmail.com>
2015-11-21 19:09:24 +0700

    StandardJS Style

--- blogel.js
+++ blogel.js
@@ -1,14 +1,12 @@
-'use babel';
+'use strict'
 
-'use strict';
+const TEMP_DIR = './tmp'
+const SERVER_DIR = './server'
 
-const TEMP_DIR = './tmp';
-const SERVER_DIR = './server';
-
-if (process.argv.length > 2) {
-   if(process.argv.indexOf('--use-babel') != -1) {
-       module.exports = require(`${TEMP_DIR}/pring`);
-   } else {
-       module.exports = require(`${SERVER_DIR}/pring`);
-   }
+if (process.argv.length > 1) {
+  if (process.argv.indexOf('--use-babel') !== -1) {
+    module.exports = require(`${TEMP_DIR}/pring`)
+  } else {
+    module.exports = require(`${SERVER_DIR}/pring`)
+  }
 }
