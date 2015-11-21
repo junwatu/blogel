60516e22a4f0a66cb08b00e9fb4b31566aa9d8c0
Equan Pr <equan.p@gmail.com>
2015-11-21 19:12:32 +0700

    Change exports style

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,22 +1,27 @@
+/* @flow */
+
 'use babel'
+
 'use strict'
 
 const moment = require('moment')
 const app = require('../../package.json')
 
 import type { Post, PostCreated } from '../core/types.js'
+
+import { savePost, getAllPost, getPostById } from '../db.js'
 import { compile } from '../core/compile'
-import { savePost, getAllPost } from '../db.js'
 import { Logger } from '../logger'
 
-exports.default = (req, res) => {
-  res.render('index', {
-    "title": app.name,
-    "version": app.version
-  })
+function defaultRoute (req:any, res:any) {
+  res.render('index', { "title": app.name, "version": app.version })
 }
 
-exports.user = (req, res) => {
+function api (req: any, res: any) {
+  res.json({ api: app.version, description: app.description })
+}
+
+function user (req:any, res:any) {
   getAllPost().then((result) => {
     res.render('list', {
       "title": app.name,
@@ -27,83 +32,97 @@ exports.user = (req, res) => {
   }, (err) => console.log(err))
 }
 
-exports.savePost = (req, res) => {
-    let doc:Post = {
-        title: req.body.post.title.value,
-        content: req.body.post.content.value,
-        postCreated : new Date(),
-        postPublished: '',
-        lastUpdated: new Date(),
-        status: req.body.post.status,
-        author: '',
-        tags: ['hello', 'world']
-    };
+function saveNewPost (req:any, res:any) {
+  let now = new Date()
+  let doc:Post = {
+    title: req.body.post.title.value,
+    content: req.body.post.content.value,
+    postCreated : now.toString(),
+    postPublished: '',
+    lastUpdated: now.toString(),
+    status: req.body.post.status,
+    author: '',
+    tags: ['hello', 'world']
+  };
 
-    savePost(doc).then((result) => {
-        compile(doc).then((status) => {
-            let jsonOut = {
-                post: status,
-                id: result.generated_keys
-            }
-            res.json(jsonOut);
-        }, (err) => {
-            res.json({ error: err });
-        })
-    }, (err) => {
-        res.json({ error: err });
-    });
-
+  savePost(doc).then((result) => {
+    compile(doc).then((status) => {
+      let jsonOut = { post: status, id: result.generated_keys }
+      res.json(jsonOut)
+    }, (err) => res.json({ error: err }))
+    }, (err) => res.json({ error: err }))
 }
 
-exports.api = (req, res) => {
-    res.json({
-        api: app.version,
-        description: app.description
-    })
+function listPosts (req:any, res:any) {
+  getAllPost().then((result) => {
+    res.json(result)  
+  }, (err) => console.log(err))
 }
 
-exports.listPosts = (req, res) => {
+function listDraftPosts (req:any, res:any) {
     res.json({
-        process: 'list posts'
+        process: 'list draft posts'
     });
 }
 
-exports.getPost = (req, res) => {
+function listPublishedPosts (req:any, res:any) {
     res.json({
-        process: 'get post'
+        process: 'list published posts'
     });
 }
 
-exports.updatePost = (req, res) => {
+function getPost (req:any, res:any) {
+  let postId = req.params.id
+  getPostById(postId).then((result) => {
+    res.json(result)
+  })
+}
+
+function updatePost (req:any, res:any) {
     res.json({
         process: 'update post'
     });
 }
 
-exports.deletePost = (req, res) => {
+function deletePost (req:any, res:any) {
     res.json({
         process: 'delete post'
     });
 }
 
-exports.loginPage = (req, res) => {
-    res.render('login', {
-        title: app.name,
-        version: app.version,
-        message: req.flash('loginMessage')
-    });
+function loginPage (req:any, res:any) {
+  res.render('login', {
+    title: app.name,
+    version: app.version,
+    message: req.flash('loginMessage')
+  });
 }
 
-exports.signupPage = (req, res) => {
-    res.render('signup', {
-        title: app.name,
-        version: app.version,
-        message: req.flash('signupMessage')
-    });
-}
-exports.logout = (req, res) => {
-    req.logout();
-    res.redirect('/');
+function signupPage (req:any, res:any) {
+  res.render('signup', {
+    title: app.name,
+    version: app.version,
+    message: req.flash('signupMessage')
+  })
 }
 
+function logout (req:any, res:any) {
+  req.logout()
+  res.redirect('/')
+}
 
+module.exports = {
+  defaultRoute,
+  user,
+  saveNewPost,
+  api,
+  listPosts,
+  listDraftPosts,
+  listPublishedPosts,
+  getPost,
+  updatePost,
+  deletePost,
+  loginPage,
+  signupPage,
+  logout
+}
