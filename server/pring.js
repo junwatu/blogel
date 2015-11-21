927f63d158f989b0e591ddea650cd59952d33793
Equan Pr <equan.p@gmail.com>
2015-11-21 19:36:16 +0700

    Use Router()

--- server/pring.js
+++ server/pring.js
@@ -2,22 +2,29 @@
 
 'use strict'
 
+import { defaultRoute, user, api, saveNewPost, listPosts, getPost, updatePost, deletePost, loginPage, signupPage, logout } from './routes'
+
 const express = require('express')
-const pring = express()
 const config = require('./config')
-const app = require('../package.json')
 const engine = require('consolidate')
 const root = require('./util')
 const passport = require('passport')
 const flash = require('connect-flash')
-const rest = require('./rest')
 const Logger = require('./logger').Logger
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
 
+const pring = express()
+const router = express.Router()
+
 require('./middleware/auth/passport')(passport)
+
+function isLoggedin (req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect('/')
+}
 
 pring.use(bodyParser.urlencoded({extended: true}))
 pring.use(bodyParser.json())
@@ -38,16 +45,40 @@ pring.set('views', `${root.path()}/views`)
 pring.engine('html', engine.handlebars)
 pring.set('view engine', 'html')
 
-rest(pring, passport)
+config.get('passport:authentication') ? pring.get('/', defaultRoute) : pring.get('/', user)
 
-pring.listen(config.get('express:port'), () => {
-  let msg = `Blogel is running on port ${config.get('express:port')}`
-  console.log(msg)
-  Logger.info(msg)
-})
+router.route('/').get(api)
 
-process.on('unhandledRejection', function (err, p) {
-  Logger.error(err.stack)
-})
+router.route('/posts')
+  .post(saveNewPost)
+  .get(listPosts)
 
-module.exports = pring
+router.route('/posts/:id')
+  .get(getPost)
+  .put(updatePost)
+  .delete(deletePost)
+
+pring.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/user',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+pring.get('/login', loginPage)
+
+pring.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/user',
+  failureRedirect: '/signup',
+  failureFlash: true
+}))
+pring.get('/signup', signupPage)
+
+pring.get('/logout', logout)
+pring.get('/user', isLoggedin, user)
+
+pring.use('/api', router)
+
+pring.listen(config.get('express:port'), () => console.log(`Blogel is running on port ${config.get('express:port')}`))
+
+process.on('unhandledRejection', (err) => Logger.error(err.stack))
+
+module.exports = { express, pring }
