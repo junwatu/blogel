497486bc7b4b125c44f7a2c80d5d0c0b88e32bcf
Equan Pr <equan.p@gmail.com>
2015-11-28 11:03:23 +0700

    copy t3-2.0.2.js to public js folder

--- scripts/copy-assets.sh
+++ scripts/copy-assets.sh
@@ -5,3 +5,8 @@ if [ ! -d 'public/css/themes' ] ; then
 fi
 cp -v ./node_modules/medium-editor/dist/css/medium-editor.css public/css
 cp -rvf ./node_modules/medium-editor/dist/css/themes/default.css public/css/themes
+
+if [ ! -d 'public/js' ] ; then
+    mkdir -p public/js/vendor
+fi
+cp -v ./node_modules/t3js/dist/t3-2.0.2.js public/js/vendor
