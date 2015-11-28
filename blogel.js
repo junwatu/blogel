553818f0003c83ae18af3ea45fd778bb401bc2e6
Equan Pr <equan.p@gmail.com>
2015-11-28 19:06:22 +0700

    Now run throug babel-node

--- blogel.js
+++ blogel.js
@@ -1,12 +1,5 @@
 'use strict'
 
-const TEMP_DIR = './tmp'
 const SERVER_DIR = './server'
 
-if (process.argv.length > 1) {
-  if (process.argv.indexOf('--use-babel') !== -1) {
-    module.exports = require(`${TEMP_DIR}/pring`)
-  } else {
-    module.exports = require(`${SERVER_DIR}/pring`)
-  }
-}
+module.exports = require(`${SERVER_DIR}/pring`)
