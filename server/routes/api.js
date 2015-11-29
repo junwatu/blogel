a7a0ee6c6c04abc06f27a2b8a3c3600a412201f8
Equan Pr <equan.p@gmail.com>
2015-11-29 09:01:10 +0700

    Move blogel API to it's own file

---
+++ server/routes/api.js
@@ -0,0 +1,59 @@
+/* @flow */
+'use strict'
+
+import moment from 'moment'
+import type { Post, PostCreated } from '../core/types.js'
+import { compile } from '../core/compile'
+import { Logger } from '../logger'
+import { savePost, getAllPost, getPostById, updateThePost } from '../db.js'
+
+const app = require('../../package.json')
+const striptags = require('striptags')
+
+export default class Api {
+  constructor () {}
+  
+  info (req: any, res: any) {
+    res.json({ api: app.version, description: app.description })
+  }
+
+  savePostAPI (req: any, res: any) {
+    let now = new Date()
+    let doc: Post = {
+      title: req.body.post.title.value,
+      content: req.body.post.content.value,
+      postCreated : now.toString(),
+      postPublished: '',
+      lastUpdated: now.toString(),
+      status: req.body.post.status,
+      author: '',
+      tags: ['hello', 'world']
+    }
+
+    savePost(doc).then((result) => {
+      compile(doc).then((status) => {
+        let jsonOut = { post: status, id: result.generated_keys }
+        res.json(jsonOut)
+      }, (err) => res.json({ error: err }))
+    }, (err) => res.json({ error: err }))
+  }
+
+  updatePostAPI (req: any, res: any) {
+    let now = new Date()
+    let doc: Post = {
+      title: req.body.post.title.value,
+      content: req.body.post.content.value,
+      postCreated : now.toString(),
+      postPublished: '',
+      lastUpdated: now.toString(),
+      status: req.body.post.status,
+      author: '',
+      tags: ['hello', 'world'],
+      generated_keys: req.params.id
+    }
+  
+    updateThePost(doc).then((result) => res.json(result))
+  }
+
+  
+}
