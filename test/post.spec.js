8e6e7c6cf708f4b369d4bd5605ea0a7abbb7f536
Equan Pr <equan.p@gmail.com>
2015-12-05 09:05:41 +0700

    Add get all post test

--- test/post.spec.js
+++ test/post.spec.js
@@ -31,38 +31,47 @@ describe('Blogel Database Test', function(){
 
     it('Should save new post', function(done){
        db.savePost(post).then(function(status){
-	       post_id = status.generated_keys[0];
-	       done();
+	       post_id = status.generated_keys[0]
+	       done()
        }, function(err){
-           done(err);
+         done(err)
        });
     })
 
     it('Should get post data base on id', function(done){
        db.getPostById(post_id).then(function(data){
-	        assert.equal(data.title,post.title, 'post title should be '+post.title);
-	        done();
+	       assert.equal(data.title,post.title, 'post title should be '+post.title);
+	       done()
        }, function(err){
-          done(err);
+         done(err)
        });
     });
 
     it('Should update post', function(){
        post_update.generated_keys = [post_id];
        db.updateThePost(post_update).then(function(data){
-           assert.equal(1, data.replaced, 'replaced (updated) should be 1');
-	   done();
+         assert.equal(1, data.replaced, 'replaced (updated) should be 1')
+	       done()
        }, function(err){
-           done(err);
+         done(err)
        });
+    })
+    
+    it('Should get all post', function() {
+      db.getAllPost().then(function(data) {
+        assert.equal('draft', data[0].status, 'status should be draft')
+        done()
+      }, function(err) {
+        done(err)
+      })
     })
 
     it('Should delete post with specified id', function(done){
        db.deletePost(post_id).then(function(result){
-	        assert.equal(1, result.deleted, 'deleted should be 1');
-	        done();
+	        assert.equal(1, result.deleted, 'deleted should be 1')
+	        done()
        }, function(err){
-           done(err);
+           done(err)
        })
     })
 })
