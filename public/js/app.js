16cf55b0e6307b7cd45e84c2c143cfa77e9bbfd0
Equan Pr <equan.p@gmail.com>
2015-12-13 20:53:00 +0700

    Make editor as global reference

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
-Box.Application.init({
-  debug: true
+'use strict'
+var MediumEditor = require('medium-editor')
+
+window.editor = new MediumEditor(document.querySelectorAll('.editable'), {
+  toolbar: {
+    allowMultiParagraphSelection: true,
+    buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'pre'],
+    diffLeft: 0,
+    diffTop: -10,
+    firstButtonClass: 'medium-editor-button-first',
+    lastButtonClass: 'medium-editor-button-last',
+    standardizeSelectionStart: false,
+    static: false,
+    align: 'center',
+    sticky: false,
+    updateOnEmptySelection: false
+  },
+  paste: {
+    cleanPastedHTML: true,
+    forcePlainText: false
+  }
 })
--- public/js/blogelweb.js
+++ public/js/blogelweb.js
@@ -1,60 +1,22 @@
 'use strict'
 
-var MediumEditor = require('medium-editor')
-var editor = new MediumEditor(document.querySelectorAll('.editable'), {
-  toolbar: {
-    allowMultiParagraphSelection: true,
-    buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3', 'quote', 'pre'],
-    diffLeft: 0,
-    diffTop: -10,
-    firstButtonClass: 'medium-editor-button-first',
-    lastButtonClass: 'medium-editor-button-last',
-    standardizeSelectionStart: false,
-    static: false,
-    align: 'center',
-    sticky: false,
-    updateOnEmptySelection: false
-  },
-  paste: {
-    cleanPastedHTML: true,
-    forcePlainText: false
-  }
-})
-
 var service = Box.Application.getService('post')
-
 var idStatus = document.getElementById('status')
 
 if (idStatus) {
   var postId = idStatus.getAttribute('title')
-
-  service.getPostById(postId).then(function (data) {
-    editor.setContent(data.post.title, 0)
-    editor.setContent(data.post.content, 1)
-  }, function (error) {
-    console.log(error)
-  })
+  if(postId){
+    service.getPostById(postId).then(function (data) {
+      editor.setContent(data.post.title, 0)
+      editor.setContent(data.post.content, 1)
+    }, function (error) {
+      console.log(error)
+    })
+  }
 }
 
 function onPublish (event) {
   console.log('publish-btn clicked!')
-}
-
-function onSave (event) {
-  var content = editor.serialize()
-  var post_content = {
-    post: {
-      title: content['element-0'],
-      content: content['element-1'],
-      status: 'draft'
-    }
-  }
-
-  service.savePost(post_content).then(function (data) {
-    console.log(data)
-  }, function (error) {
-    console.log(error)
-  })
 }
 
 function onUpdate (event) {
@@ -69,6 +31,8 @@ function onUpdate (event) {
       id: postId
     }
   }
+
+  console.log(post_content_update)
 
   service.updatePost(post_content_update).then(function (data) {
     console.log(data)
@@ -88,7 +52,6 @@ function onDeleteDoc (event) {
 }
 
 $('#publish-btn').on('click', onPublish)
-$('#save-btn').on('click', onSave)
 $('#update-btn').on('click', onUpdate)
 $('#setting-btn').on('click', onSetting)
 $('#trash-btn').on('click', onDeleteDoc)
